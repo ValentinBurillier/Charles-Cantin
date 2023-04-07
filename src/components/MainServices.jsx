049098668c2b24c270ee8dffbb6640040f7df1d2
Valentin Burillier <valentin.burillier@gmail.com>
2023-04-07 10:39:14 +0200

    end write all service html, css, responsive and add background button on service click. NEXT STEP : fix bug service two click for display image

--- src/components/MainServices.jsx
+++ src/components/MainServices.jsx
@@ -6,14 +6,14 @@ const MainServices = (props) => {
     <main className={styles.Main}>
       <h2 className={styles.Main__title}>"{props.title}"</h2>
       <p className={styles.Main__description}>{props.description}</p>
-      <p className={styles.Main__price}>{props.price}</p>
+      <p className={styles.Main__price}>{props.price}€</p>
       <div className={styles.Main__container}>
-        <button className={styles.Container__btn} id={styles.Btn1} data-nb="1"></button>
-        <button className={styles.Container__btn} id={styles.Btn2} data-nb="2"></button>
-        <button className={styles.Container__btn} id={styles.Btn3} data-nb="3"></button>
-        <button className={styles.Container__btn} id={styles.Btn4} data-nb="4"></button>
-        <button className={styles.Container__btn} id={styles.Btn5} data-nb="5"></button>
-        <button className={styles.Container__btn} id={styles.Btn6} data-nb="6"></button>
+        <button className={styles.Container__btn} id={styles.Btn1} data-nb="0"></button>
+        <button className={styles.Container__btn} id={styles.Btn2} data-nb="1"></button>
+        <button className={styles.Container__btn} id={styles.Btn3} data-nb="2"></button>
+        <button className={styles.Container__btn} id={styles.Btn4} data-nb="3"></button>
+        <button className={styles.Container__btn} id={styles.Btn5} data-nb="4"></button>
+        <button className={styles.Container__btn} id={styles.Btn6} data-nb="5"></button>
       </div>
       <ul className={styles.Container__icons}>
           <li>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -7,38 +7,130 @@ import styles from './Services.module.css'
 
 const Services = () => {
   const [data, setData] = useState('')
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('');
   const [price, setPrice] = useState('')
+  const [imgSm, setImgSm] = useState('');
+  const [imgM, setImgM] = useState('');
+  const [imgL, setImgL] = useState('');
 
+  /* GET BDD  ALL DATA (VALIDE) STOCK IN CONST DATA */
   useEffect(() => {
-    fetch('http://localhost:1337/api/services-p/1?populate=*')
+    fetch('http://localhost:1337/api/services-p?populate=*')
       .then(response => response.json())
-      .then(json => setData(json.data.attributes))
+      .then(json => setData(json.data))
       .catch(error => console.log(error));
   }, [])
 
-  // const urlImage = data.image.data.attributes.url;
+  /* EVENT ON CLICK BUTTON TO CHANGE SERVICE AND GET DATA*/
+  const allBtn = [...document.querySelectorAll('button')];
+  allBtn.forEach(element => element.addEventListener('click', () => {
+    var btnDataSet = element.dataset.nb;
+    getDataService(data, btnDataSet)
+  }))
 
-    const bgBody = document.getElementsByTagName('body')[0];
+  /* FUNCTION GET DATA OF THE SERVICE */
+  function getDataService(data, btnDataSet) {
+    setTitle(data[btnDataSet].attributes.title);
+    setDescription(data[btnDataSet].attributes.description);
     
-  // DISPLAY GOOD IMAGE RESOLUTION UNDER THE SIZE SCREEN
-    function imgTest() {
+    if(data[btnDataSet].attributes.price > 0) {
+      setPrice(data[btnDataSet].attributes.price);
+    } else {
+      setPrice('Sur mesure');
+    }
 
+    setImgSm(data[btnDataSet].attributes.image.data[2].attributes.url)
+    setImgM(data[btnDataSet].attributes.image.data[1].attributes.url)
+    setImgL(data[btnDataSet].attributes.image.data[0].attributes.url)
+    imgTest(btnDataSet);
+  }
+  // const urlImage = data.image.data.attributes.url;
+    const bgBody = document.getElementsByTagName('body')[0];
+
+  // DISPLAY GOOD IMAGE RESOLUTION UNDER THE SIZE SCREEN
+    function imgTest(btnDataSet) {
       let mediaBase = window.matchMedia("(max-width: 799px)");
+      
       if (mediaBase.matches) {
-        bgBody.style.backgroundImage = "url('http://localhost:1337/uploads/service1_mobile_6c0f0e7e97.jpg')";
+        let urlImgSm = `url('http://localhost:1337${imgSm}')`; // GOOD URL
+        bgBody.style.backgroundImage = urlImgSm; // GOOT SYNTAXE BUT NOT DISPLAY
+        bgButtonClick(btnDataSet);
       }
 
       let media800 = window.matchMedia("(min-width: 800px) and (min-height: 568px");
       if (media800.matches) {
-        bgBody.style.backgroundImage = "url('http://localhost:1337/uploads/service1_m_6aad76b042.jpg')";
+        let urlImgM = `url('http://localhost:1337${imgM}')`
+        bgBody.style.backgroundImage = urlImgM;
+        bgButtonClick(btnDataSet);
       }
-
+      
       let media1200 = window.matchMedia("(min-width: 1200px) and (min-height: 568px");
       if (media1200.matches) {
-        bgBody.style.backgroundImage = "url('http://localhost:1337/uploads/service1_l_72e03f71a9.jpg')";
+        let urlImgL = `url('http://localhost:1337${imgL}')`;
+        bgBody.style.backgroundImage = urlImgL;
+        let root = document.querySelector('body > div');
+        bgButtonClick(btnDataSet);
       }
     }
 
+    /* FUNCTION BACKGROUND BUTTON ON SERVICE CLIK*/
+    function bgButtonClick(btnDataSet) {
+      switch(btnDataSet) {
+        case "0":
+          bgBody.style.backgroundPositionY = 'center';
+          allBtn[0].style.backgroundColor = 'white';
+          allBtn[1].style.backgroundColor = 'gray';
+          allBtn[2].style.backgroundColor = 'gray';
+          allBtn[3].style.backgroundColor = 'gray';
+          allBtn[4].style.backgroundColor = 'gray';
+          allBtn[5].style.backgroundColor = 'gray';
+          break;
+        case "1":
+          bgBody.style.backgroundPositionY = 'center';
+          allBtn[0].style.backgroundColor = 'gray';
+          allBtn[1].style.backgroundColor = 'white';
+          allBtn[2].style.backgroundColor = 'gray';
+          allBtn[3].style.backgroundColor = 'gray';
+          allBtn[4].style.backgroundColor = 'gray';
+          allBtn[5].style.backgroundColor = 'gray';
+          break;
+        case "2":
+          allBtn[0].style.backgroundColor = 'gray';
+          allBtn[1].style.backgroundColor = 'gray';
+          allBtn[2].style.backgroundColor = 'white';
+          allBtn[3].style.backgroundColor = 'gray';
+          allBtn[4].style.backgroundColor = 'gray';
+          allBtn[5].style.backgroundColor = 'gray';
+        break;
+        case "3":
+          allBtn[0].style.backgroundColor = 'gray';
+          allBtn[1].style.backgroundColor = 'gray';
+          allBtn[2].style.backgroundColor = 'gray';
+          allBtn[3].style.backgroundColor = 'white';
+          allBtn[4].style.backgroundColor = 'gray';
+          allBtn[5].style.backgroundColor = 'gray';
+        break;
+        case "4":
+          allBtn[0].style.backgroundColor = 'gray';
+          allBtn[1].style.backgroundColor = 'gray';
+          allBtn[2].style.backgroundColor = 'gray';
+          allBtn[3].style.backgroundColor = 'gray';
+          allBtn[4].style.backgroundColor = 'white';
+          allBtn[5].style.backgroundColor = 'gray';
+        break;
+        case "5":
+          allBtn[0].style.backgroundColor = 'gray';
+          allBtn[1].style.backgroundColor = 'gray';
+          allBtn[2].style.backgroundColor = 'gray';
+          allBtn[3].style.backgroundColor = 'gray';
+          allBtn[4].style.backgroundColor = 'gray';
+          allBtn[5].style.backgroundColor = 'white';
+        break;
+        default:
+          return;
+      }
+    }
     window.addEventListener('resize', imgTest);
     imgTest();
 
@@ -46,9 +138,9 @@ const Services = () => {
     <>
       <Header />
       <MainServices 
-        title = {`${data.title}`}
-        description = {`${data.description}`}
-        price = {`${data.price}€`}
+        title={title}
+        description={description}
+        price={price}
       />
     </>
   )
